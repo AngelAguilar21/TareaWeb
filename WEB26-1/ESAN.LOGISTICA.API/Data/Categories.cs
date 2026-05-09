f903ddb055ee0576238a64fc0bb9ffb4a2d82d85
Angel Aguilar Contreras <168390286+AngelAguilar21@users.noreply.github.com>
2026-05-08 23:32:27 -0500

    Add files via upload

---
+++ WEB26-1/ESAN.LOGISTICA.API/Data/Categories.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ESAN.LOGISTICA.API.Data;
+
+public partial class Categories
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    // Relación inversa
+    public virtual ICollection<Products> Products { get; set; } = new List<Products>();
+}
---
+++ WEB26-1/ESAN.LOGISTICA.API/Data/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ESAN.LOGISTICA.API.Data;
+
+public partial class Products
+{
+    public int Id { get; set; }
+
+    public string? Description { get; set; }
+
+    public decimal? UnitPrice { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    // Relación con Categories
+    public virtual Categories? Category { get; set; }
+}
